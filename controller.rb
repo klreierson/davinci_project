48849d8f079383d6ca7c78264a07799471c832b4
Kyle Reierson <klreierson@yahoo.com>
2013-08-24 03:31:48 -0600

    Update to Project
    Added seeds to db and layout.erb

--- controller.rb
+++ controller.rb
@@ -1,9 +1,40 @@
-require 'sinatra'
+require './dvc-sinatra'
 
-get "/home" do
-  halt erb(:single_page)
+get "/" do
+  halt erb(:index)
 end
 
-get "/livevideo" do
-  halt erb(:two_page)
+get "/company/:symbol" do
+	@name = Company.where(name:"name").first
+	@symbol = Company.where(symbol:params["symbol"]).first
+	@price 	= MarketBeat.last_trade_real_time params["symbol"]
+	@change = MarketBeat.change_and_percent_change params["symbol"]
+	@volume = MarketBeat.volume params["symbol"]
+	@days_range = MarketBeat.days_range params["symbol"]
+	@eps = MarketBeat.earnings_to_share params["symbol"]
+	@pe = MarketBeat.pe_ratio params["symbol"]
+	@stock_exchange = MarketBeat.stock_exchange params["symbol"]
+
+
+	market_cap = MarketBeat.market_capitalization params["symbol"]
+	# @market_cap is rounded to billions
+	@market_cap = market_cap.to_i / 1000
+  halt erb(:company)
 end
+
+post "/company/:symbol" do
+	@name = Company.where(name:"name").first
+	@symbol = Company.where(symbol:params["symbol"]).first
+	@price 	= MarketBeat.last_trade_real_time params["symbol"]
+	@change = MarketBeat.change_and_percent_change params["symbol"]
+	@days_range = MarketBeat.days_range params["symbol"]
+  halt erb(:company)
+end
+
+get "/profile" do
+  @profile = Profile.first
+  halt erb(:profile)
+end
+
+
+
---
+++ dvc-sinatra.rb
@@ -0,0 +1,51 @@
+require 'active_record'
+require 'logger'
+require 'sinatra'
+require 'yaml'
+require 'action_view' # for Rails form helpers
+require 'erubis' # for escaping of HTML
+require 'will_paginate/active_record' if defined? WillPaginate
+
+
+# Escape HTML
+set :erb, escape_html: true
+
+# Enable sessions -- note that a web server restart will invalidate all cookies
+use Rack::Session::Cookie, secret: SecureRandom.hex
+
+# for Rails form helpers
+helpers ActionView::Helpers::FormTagHelper
+helpers ActionView::Helpers::FormOptionsHelper
+helpers ActionView::Helpers::DynamicForm if defined? ActionView::Helpers::DynamicForm
+
+# avoid error message that we get from using Rails form helpers with Sinatra
+helpers do
+  def protect_against_forgery?
+    false
+  end
+end
+
+# Comment this line to turn off Active Record SQL logging
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+if ENV['DATABASE_URL'] # for Heroku
+  db = URI.parse(ENV['DATABASE_URL'])
+  ActiveRecord::Base.establish_connection({
+    adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host:     db.host,
+    port:     db.port,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8',
+  })
+elsif Dir.exists?('db') # for local development
+  config = YAML.load_file('config/database.yml')
+  ActiveRecord::Base.establish_connection(config['development'])
+
+  after { ActiveRecord::Base.connection.close }
+  # fix ConnectionTimeoutError
+end
+
+# Load models from models directory, like Rails
+Dir['./models/*.rb'].each { |file| require file }
